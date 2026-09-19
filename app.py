fb2bb452f4174048b1de1992268ef575e5dfe7a5
André Luferat <contato@luferat.net>
2026-09-18 21:46:39 -0300

    bsok

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 # Importando as dependências
-from flask import Flask
+from flask import Flask, render_template
 
 # Inicializar variáveis e componentes
+
+# Nome do aplicativo (Site da Web) → global
+sitename = "My Flask"
 
 # Inicializa o plaicativo Flask (HTTP)
 app = Flask(__name__)
@@ -9,14 +12,21 @@ app = Flask(__name__)
 
 # Rota da página inicial (rota raiz ou root)
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template(
+        "home.html",
+        sitename=sitename
+    )
 
 # Uma rota bem simples
+
+
 @app.route("/about")
 def about():
-    output = "Sobre nóis...."
-    return output
+    return render_template(
+        'about.html',
+        sitename=sitename
+    )
 
 
 # Ativa o modo DEBUG e o main loop no localhost
